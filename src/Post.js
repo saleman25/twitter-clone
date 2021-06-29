60d7e10a885de06524287568a8e9f5d722551103
Samantha Aleman <samantha.aleman25@gmail.com>
2021-06-28 23:24:14 -0700

    started adding post css

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Post.css';
 import { Avatar } from '@material-ui/core';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser'
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
 function Post({ 
     displayName, 
@@ -24,8 +28,9 @@ function Post({
                     <div className='post_headerText'>
                             <h3>
                                 Sam {" "}
-                                <span>
+                                <span className='post_headerSpecial'>
                                     <VerifiedUserIcon className='post_badge' />
+                                    @sam
                                 </span>
                             </h3>
                     </div>
@@ -34,6 +39,12 @@ function Post({
                     </div>
                 </div>
                 <img src='https://media4.giphy.com/media/WEiKBTaESHHhK/200.gif' alt=''/>
+            <div className= "post_footer">
+                <ChatBubbleOutlineIcon fontSize='small' />
+                <RepeatIcon fontSize='small' />
+                <FavoriteBorderIcon fontSize='small' />
+                <PublishIcon fontSize='small' />
+            </div>
             </div>
 
         </div>
