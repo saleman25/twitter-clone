d1cbf91379b761b03222a42beac3b4160e017cd6
Samantha Aleman <samantha.aleman25@gmail.com>
2021-06-28 19:03:44 -0700

    started building out sidebar componenets

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>hey :3</h1>
+
+      <Sidebar />
+      {/* Feed */}
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <TwitterIcon />
+            {/* SidebarOption*/}
+            {/* Button -> tweet*/}
+        </div>
+    )
+}
+
+export default Sidebar
