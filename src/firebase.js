130d3121307858d3692de5efb7eb2cf1d48e3357
Samantha Aleman <samantha.aleman25@gmail.com>
2021-06-29 21:53:11 -0700

    starting to incorporate firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5yG2lmO2nNZUDxgrKQ6Z-MERRAO9Mcs8",
+    authDomain: "twitter-clone-45cc6.firebaseapp.com",
+    projectId: "twitter-clone-45cc6",
+    storageBucket: "twitter-clone-45cc6.appspot.com",
+    messagingSenderId: "835783985558",
+    appId: "1:835783985558:web:25deed54ab271edb33b5b7",
+    measurementId: "G-EJZGTZHN3F"
+  };
